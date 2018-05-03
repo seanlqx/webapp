3e3f719e2a91a9b5b1b2217f32386a77890835dc
seanlqx <sean_lqx@126.com>
2018-05-03 17:19:05 +0800

    照着敲完了代码

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
---conding: utf-8 --
+#!/usr/bin/env python3
+# -*-coding: utf-8 -*-
+
+_author_='Seanlqx'
+
+#编写Web App骨架
 
 import logging; logging.basicConfig(level=logging.INFO)
 
@@ -7,6 +12,18 @@ from datetime import datetime
 
 from aiohttp import web
 
-def index(request)
+#制作响应函数
+def index(request):
     return web.Response(body=b'<h1>Awesome</h1>')
 	
+@asyncio.coroutine
+def init(loop):#Web app服务器初始化
+    app = web.Application(loop=loop) #制作响应函数集合
+	app.router.add_route('GET', '/', index)
+	srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+	logging.info('server started at http://127.0.0.1:9000...')
+	return srv
+	
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
